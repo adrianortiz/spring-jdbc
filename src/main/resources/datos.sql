0015032ffd7c163d9257369e555f202f03a89943
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2017-01-15 17:16:15 -0600

    Create project
    Spring JDBC
     - Config Spring JDBC
     - Test Spring JDBC

---
+++ src/main/resources/datos.sql
@@ -0,0 +1,3 @@
+insert into persona (nombre, ape_paterno, ape_materno, email) values ('Admin', 'admin', 'ortiz', 'admin@codizer.com');
+insert into persona (nombre, ape_paterno, ape_materno, email) values ('Juan', 'Perez', 'gomez', 'juan@codizer.com');
+insert into persona (nombre, ape_paterno, ape_materno, email) values ('Juan', 'Rodriguez', 'gonzales', 'jrodriguez@gmail.com');
